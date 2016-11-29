d3e45bbe2e4c2ca3246a36ee6de1199f694730c3
dichenko <m@dichenko.ru>
2016-11-29 15:10:42 +0300

    class

--- Bioinformatics Institute/1.6.7.py
+++ Bioinformatics Institute/1.6.7.py
@@ -1,3 +1,9 @@
+def obrabotka(x):
+    """
+    :param x: строка вида A : B C
+    :return: k = A, (B,C)
+    """
+    t,y = x.split(':')
 n = int(input())
 cl = []
 for i in range(n):
@@ -8,8 +14,10 @@ qw = []
 for i in range(q):
     qw.append(input())
 parents = {}
-cildren = {}
+children = {}
 for el in cl:
     if len(el) == 1:
+        parents[el] = 0
+    else
 
 
