11a8c10bdf84c14956a396ba52c7f55f37e98bce
dichenko <m@dichenko.ru>
2016-11-30 19:24:14 +0300

    step =)

--- Bioinformatics Institute/1.6.7.py
+++ Bioinformatics Institute/1.6.7.py
@@ -1,8 +1,16 @@
 def find_path(graph, start, end, path=[]):
+    """
+    http://www.infocity.kiev.ua/prog/python/content/pytonesse_3.shtml
+    :param graph:
+    :param start:
+    :param end:
+    :param path:
+    :return:
+    """
     path = path + [start]
     if start == end:
         return path
-    if not graph.has_key(start):
+    if start not in graph.keys():
         return None
     for node in graph[start]:
         if node not in path:
@@ -13,30 +21,40 @@ def find_path(graph, start, end, path=[]):
 def obrabotka(x):
     """
     :param x: строка вида A : B C
-    :return: k = A, (B,C)
+    :return: k = A, [B,C]
     """
     t,y = x.split(':')
     t = t[:-1]
     y = y.split()
     return t,y
+
 n = int(input())
 cl = []
 for i in range(n):
     cl.append(input())
 
-q = int(input())
-qw = []
-for i in range(q):
-    qw.append(input())
+
 parents = {}
 children = {}
 for el in cl:
-    if len(el) == 1:
-        parents[el] = 0
+    if ':' not in el:
+        parents[el] = []
     else:
         t,y = obrabotka(el)
         parents[t] = y
-print(parents)
 
+
+
+q = int(input())
+ans = []
+for i in range(q):
+    y,x = input().split()
+    if find_path(parents,x,y):
+        ans.append('Yes')
+    else:
+        ans.append('No')
+
+for el in ans:
+    print(el)
 
 
