3a104012aad3a481af0d6596dc79b942ed0d8517
dichenko <m@dichenko.ru>
2016-11-30 09:53:40 +0300

    class

--- Bioinformatics Institute/1.6.7.py
+++ Bioinformatics Institute/1.6.7.py
@@ -1,9 +1,24 @@
+def find_path(graph, start, end, path=[]):
+    path = path + [start]
+    if start == end:
+        return path
+    if not graph.has_key(start):
+        return None
+    for node in graph[start]:
+        if node not in path:
+            newpath = find_path(graph, node, end, path)
+            if newpath: return newpath
+    return None
+
 def obrabotka(x):
     """
     :param x: строка вида A : B C
     :return: k = A, (B,C)
     """
     t,y = x.split(':')
+    t = t[:-1]
+    y = y.split()
+    return t,y
 n = int(input())
 cl = []
 for i in range(n):
@@ -18,6 +33,10 @@ children = {}
 for el in cl:
     if len(el) == 1:
         parents[el] = 0
-    else
+    else:
+        t,y = obrabotka(el)
+        parents[t] = y
+print(parents)
+
 
 
