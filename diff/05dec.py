cc3afeb4d94afa17f888d0c93a17b8bad208e3c6
dichenko <m@dichenko.ru>
2016-12-08 17:36:04 +0300

    5 dec

--- diff/05dec.py
+++ diff/05dec.py
@@ -1,6 +1,16 @@
 import hashlib
 m = hashlib.md5()
 line ="abc"
+for i in range(10000000000):
+    line1 = line+str(i)
+    m.update(line1.encode('utf-8'))
+    s = (m.hexdigest())
+    if s[:5] == '00000':
+        break
+print(line1)
+print(s)
 
-m.update(line.encode('utf-8'))
+
+
+
 print (m.hexdigest())
